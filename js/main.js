e927bd0f272956e5b64ec620a4917f5f624c9052
nurukan <chiakic0taan@gmail.com>
2025-05-13 12:30:29 +0900

    5・6個のスイッチを追加
    引数を用いたスイッチの作成

--- js/main.js
+++ js/main.js
@@ -1,38 +1,27 @@
 'use strict';
 {
 
-//   <ul id="ul__box">
-//         <!-- 関数を使用 -->
-//             <li id="box1">
-//                 <div id="light1"></div>
-//                 <button id="switch1">スイッチ１</button>
-//             </li>
 
-//             <!-- 関数を使わない -->
-//             <li id="box2">
-//                 <div id="light2"></div>
-//                 <button id="switch2">スイッチ２</button>
-//             </li>
-//         </ul>
-
-
-const box1 = document.getElementById('box1');
 const light1 = document.getElementById('light1');
 const switch1 = document.getElementById('switch1');
 
-const box2 = document.getElementById('box2');
 const light2 = document.getElementById('light2');
 const switch2 = document.getElementById('switch2');
 
-const box3 = document.getElementById('box3');
 const light3 = document.getElementById('light3');
 const switch3 = document.getElementById('switch3');
 
-const box4 = document.getElementById('box4');
 const light4 = document.getElementById('light4');
 const switch4 = document.getElementById('switch4');
 
-// 関数を使用
+const light5 = document.getElementById('light5');
+const switch5 = document.getElementById('switch5');
+
+const light6 = document.getElementById('light6');
+const switch6 = document.getElementById('switch6');
+
+
+// 関数宣言
 // ボタンを押して、実行すると「真・偽を判断して」処理する
 let LightOn1 = true;
 function isLightOn1 (){
@@ -46,7 +35,6 @@ function isLightOn1 (){
         console.log('1の部屋の電気が消えていたから、つけたよ！');
     }
 }
-
 switch1.addEventListener('click',()=> {
         isLightOn1 ();
 });
@@ -81,8 +69,8 @@ switch3.addEventListener('click',()=>{
 });
 
 
-
-// 
+// 関数宣言、実行関数を一行に簡略して記述 
+// 引数
 let LightOn4 = true;
 function isLightOn4 (){
     if(LightOn4 === true){
@@ -96,6 +84,46 @@ function isLightOn4 (){
     }
 }
 switch4.addEventListener('click', isLightOn4);
+
+
+// 関数宣言
+// 引数（複数）
+let LightOn5 = { value: true };
+function toggleLight(roomLightElement, isLightOn, roomName) {
+    if (isLightOn.value) {
+        isLightOn.value = false;
+        roomLightElement.style.backgroundColor = 'rgb(46, 45, 45)';
+        console.log(`${roomName}の電気が付いていたから、消したよ！`);
+    } else {
+        isLightOn.value = true;
+        roomLightElement.style.backgroundColor = 'rgb(255, 255, 142)';
+        console.log(`${roomName}の電気が消えていたから、つけたよ！`);
+    }
+}
+switch5.addEventListener('click', () => {
+    toggleLight(light5, LightOn5, '5の部屋');
+});
+
+
+// 関数宣言     
+// 引数（複数）
+let LightOn6 = { value: true };  
+switch6.addEventListener('click', () => {
+    function toggleLight6(roomLightElement,isLightOn,roomName){
+
+        if(isLightOn.value){
+            isLightOn.value = false;
+            roomLightElement.style.backgroundColor = 'rgb(46, 45, 45)';
+            console.log(`${roomName}の電気が付いていたから、消したよ！`);
+        }else{
+            isLightOn.value = true;
+            roomLightElement.style.backgroundColor = 'rgb(255, 255, 142)';
+            console.log(`${roomName}の電気が消えていたから、つけたよ！`);
+        }
+    }
+    toggleLight(light6, LightOn6, '６の部屋');
+});
+
 
 
 
